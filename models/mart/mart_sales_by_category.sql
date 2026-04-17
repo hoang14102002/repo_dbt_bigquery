fc7ac326747cad8734bf27475d0d6f989f1eaef0
MINH HOANG DANGG <minhhoangdang14@gmail.com>
2026-04-17 03:09:56 +0000

    fix_mart_sales_by_category

--- models/mart/mart_sales_by_category.sql
+++ models/mart/mart_sales_by_category.sql
@@ -1,10 +1,11 @@
 {{ config(materialized='table') }}
 
 select 
-    sub_category,
-    category,
-    sum(sales) as total_sales,
-    sum(profit) as total_profit,
-    count(distinct order_id) as total_orders
-from {{ref('int_orders_detail')}}
-group by category, sub_category
+    t2.sub_category,
+    t2.category,
+    sum(t1.sales) as total_sales,
+    sum(t1.profit) as total_profit,
+    count(distinct t1.order_id) as total_orders
+from {{ref('int_orders_detail')}} t1
+    left join {{ref('int_dim_product')}} t2 ON t1.product_id = t2.product_id
+group by t2.category, t2.sub_category
